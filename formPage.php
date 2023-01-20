2c49162f8b4edc29fee65f729aea3ca08c972af8
Robert Dzienio <robertdzienio+git@gmail.com>
2023-01-20 20:01:35 +0100

    pure css added

--- formPage.php
+++ formPage.php
@@ -1,27 +1,55 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Formularz kontaktowy</title>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="styles.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+<title>Zostaw kontakt | Pożyczki bez BIK</title>
+<link rel="stylesheet" href="styles.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/purecss@3.0.0/build/pure-min.css" integrity="sha384-X38yfunGUhNzHpBaEBsWLO+A0HDYOQi8ufWDkZ0k9e0eXz/tH3II7uKZ9msv++Ls" crossorigin="anonymous">
 </head>
 <body>
-    <h1>Wypełnij ten formularz to skontaktujemy się z Tobą!</h1>
-    
-    <form method="post" action="send-email.php">
-        <label for="name">Imię</label>
-        <input type="text" name="name" id="name" required>
-        
-        <label for="phone">Telefon</label>
-        <input type="tel" id="phone" name="phone" placeholder="999999999" pattern="[0-9]{9}" required>
-        
-        <br>
-        
-        <button>Zapisz</button>
+<div class="topnav">
+  <a href="index.php">Strona główna</a>
+  <a href="index.php">Aktualności</a>
+  <a href="formPage.php" style="float:right; color:rgb(190, 0, 0); background-color: rgba(170, 170, 170, 1); font-weight: bold"> zadzwoń: 517-883-411</a>
+</div>
+<div class="header">
+  <h1>Pożyczki bez BIK</h1>
+</div>
+
+
+
+<div class="row">
+  <div class="formcolumn">
+    <div class="card">
+    <form class="pure-form pure-form-aligned" method="post" action="send-email.php">
+        <fieldset>
+            <legend>Zostaw kontakt do siebie, a oddzwonimy!</legend>
+            <div class="pure-control-group">
+                <label for="name">Imię i nazwisko</label>
+                <input type="text" name="name" id="name" required>
+            </div>
+            <div class="pure-control-group">
+                <label for="phone">Telefon</label>
+                <input type="tel" id="phone" name="phone" placeholder="999999999" pattern="[0-9]{9}" required>
+            </div>
+            <div class="pure-control-group">
+                <label for="regulamin" class="pure-checkbox">
+                    <input type="checkbox" id="regulamin" required/> Tak, zapoznałem się z regulaminem
+                </label>
+            </div>
+            <div class="pure-control-group">
+                <button class="pure-button pure-button-primary" type="submit">Zapisz</button>
+            </div>
+        </fieldset>
     </form>
-    <br>
-    <br>
-    <p>Powrót do strony <a href="index.php">głównej</a></p>
+      </div>
+  </div>
+</div>
+
+<div class="footer">
+    <h2>Zostaw kontakt do siebie <a href="formPage.php">tutaj</a></h2>
+</div>
+
+
+
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Pożyczki bez BIK</title>
+<title>Zostaw kontakt | Pożyczki bez BIK</title>
 <link rel="stylesheet" href="styles.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/purecss@3.0.0/build/pure-min.css" integrity="sha384-X38yfunGUhNzHpBaEBsWLO+A0HDYOQi8ufWDkZ0k9e0eXz/tH3II7uKZ9msv++Ls" crossorigin="anonymous">
 </head>
 <body>
 <div class="topnav">
@@ -17,25 +18,35 @@
 
 
 <div class="row">
-  <div class="leftcolumn">
+  <div class="formcolumn">
     <div class="card">
-    <form method="post" action="send-email.php">
-        <label for="name">Imię</label><br>
-        <input type="text" name="name" id="name" required><br>
-        <br>
-        <label for="phone">Telefon</label><br>
-        <input type="tel" id="phone" name="phone" placeholder="999999999" pattern="[0-9]{9}" required><br>
-        
-        <br>
-        
-        <button>Zapisz</button>
+    <form class="pure-form pure-form-aligned" method="post" action="send-email.php">
+        <fieldset>
+            <legend>Zostaw kontakt do siebie, a oddzwonimy!</legend>
+            <div class="pure-control-group">
+                <label for="name">Imię i nazwisko</label>
+                <input type="text" name="name" id="name" required>
+            </div>
+            <div class="pure-control-group">
+                <label for="phone">Telefon</label>
+                <input type="tel" id="phone" name="phone" placeholder="999999999" pattern="[0-9]{9}" required>
+            </div>
+            <div class="pure-control-group">
+                <label for="regulamin" class="pure-checkbox">
+                    <input type="checkbox" id="regulamin" required/> Tak, zapoznałem się z regulaminem
+                </label>
+            </div>
+            <div class="pure-control-group">
+                <button class="pure-button pure-button-primary" type="submit">Zapisz</button>
+            </div>
+        </fieldset>
     </form>
       </div>
   </div>
 </div>
 
 <div class="footer">
-    <h2>Zostaw kontakt do siebie --><a href="formPage.php">tutaj</a></h2>
+    <h2>Zostaw kontakt do siebie <a href="formPage.php">tutaj</a></h2>
 </div>
 
 
