6243ffe76c31d12e97a6fc9dec7bb1f07d5d7b01
Robert Dzienio <55928400+rdzienio@users.noreply.github.com>
2023-02-01 09:50:59 +0100

    added viewport

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/purecss@3.0.0/build/pure-min.css" integrity="sha384-X38yfunGUhNzHpBaEBsWLO+A0HDYOQi8ufWDkZ0k9e0eXz/tH3II7uKZ9msv++Ls" crossorigin="anonymous">
 <meta name="keywords" content="pożyczka online, pożyczka pozabankowa, kredyt gotówkowy">
 <meta name="description" content="Istnieje skuteczna alternatywa dla kredytu bankowego. Przekonaj się czym jest pożyczka pozabankowa i kto może z niej skorzystać.">
+<meta name="viewport" content="width=device-width,initial-scale=1">
 </head>
 <body>
 <div class="topnav">
--- sent.php
+++ sent.php
@@ -6,7 +6,8 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/purecss@3.0.0/build/pure-min.css" integrity="sha384-X38yfunGUhNzHpBaEBsWLO+A0HDYOQi8ufWDkZ0k9e0eXz/tH3II7uKZ9msv++Ls" crossorigin="anonymous">
     <meta name="keywords" content="pożyczka online, pożyczka pozabankowa, kredyt gotówkowy">
     <meta name="description" content="Aby zweryfikować swoją zdolność kredytową nie musisz ruszać się z domu – decyzję dostaniesz przez telefon">
-</head>
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+  </head>
 <body>
 <div class="topnav">
   <a href="index.php">Strona główna</a>
